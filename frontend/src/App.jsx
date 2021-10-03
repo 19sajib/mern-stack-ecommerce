eba64620e7379e52d3954db419fe9c94fc9e89da
Sajib <contact2sajib@gmail.com>
2021-10-03 17:42:24 +0600

    product-pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './Pages/Home';
+import Product from './Pages/Product';
+import ProductList from './Pages/ProductList';
 
 const App = () => {
     return (
         <div>
-            <Home />
+            <Product />
         </div>
     )
 }
