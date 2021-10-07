948c7249224dd3172f676b32fee7bdad3b863d86
Sajib <contact2sajib@gmail.com>
2021-10-07 12:03:49 +0600

    stripe implementation

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+const cors = require('cors');
 const dotenv = require('dotenv')
 const userRoute = require('./routes/user.js')
 const authRoute = require('./routes/auth')
 const productRoute = require('./routes/product')
 const cartRoute = require('./routes/cart.js')
 const orderRoute = require('./routes/order.js')
+const stripeRoute = require('./routes/stripe.js')
+
 
 dotenv.config()
 
@@ -17,7 +20,7 @@ mongoose.connect(process.env.MONGO_URL, {
 .then(()=>console.log('MongoDB connected.'))
 .catch((err)=>console.log(err))
 
-
+app.use(cors())
 app.use(express.json())
 
 //Routes
@@ -27,6 +30,7 @@ app.use('/api/auth', authRoute);
 app.use('/api/products', productRoute)
 app.use('/api/carts', cartRoute)
 app.use('/api/orders', orderRoute)
+app.use('/api/checkout', stripeRoute)
 
 
 app.listen(process.env.PORT || 5000, ()=> {
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,44 @@ import Login from './Pages/Login';
 import Product from './Pages/Product';
 import ProductList from './Pages/ProductList';
 import Register from './Pages/Register';
+import Pay from './Pages/Pay';
+import Success from './Pages/Success'
+
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
+  } from "react-router-dom";
 
 const App = () => {
     return (
-        <div>
-            <Home/>
-        </div>
+        <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList />
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/pay">
+          <Pay />
+        </Route>
+        <Route path="/success">
+          <Success />
+        </Route>
+        <Route path="/login"><Login /></Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
     )
 }
 
